75e69fcc7ccbdd9717622e93354be11b97a5b793
xuyi <kbb@qq.com>
2019-05-22 23:54:18 +0800

    lewyerlist

--- pages/about/about.js
+++ pages/about/about.js
@@ -10,30 +10,33 @@ Page({
     //     // }
     //   }
     // })
-
-    wx.saveImageToPhotosAlbum({
-      filePath: 'assets/images/qrcode.jpg',
-      success: function(data) {
-        console.log(data);
-      },
-      fail: function(err) {
-        console.log(err);
-        if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
-          console.log("用户一开始拒绝了，我们想再次发起授权")
-          console.log('打开设置窗口')
-          wx.openSetting({
-            success(settingdata) {
-              console.log(settingdata)
-              if (settingdata.authSetting['scope.writePhotosAlbum']) {
-                console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
-              } else {
-                console.log('获取权限失败，给出不给权限就无法正常使用的提示')
-              }
-            }
-          })
-        }
-      }
+    wx.navigateTo({
+      url: '/pages/lawyerlist/lawyerlist',
     })
+
+    // wx.saveImageToPhotosAlbum({
+    //   filePath: 'assets/images/qrcode.jpg',
+    //   success: function(data) {
+    //     console.log(data);
+    //   },
+    //   fail: function(err) {
+    //     console.log(err);
+    //     if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
+    //       console.log("用户一开始拒绝了，我们想再次发起授权")
+    //       console.log('打开设置窗口')
+    //       wx.openSetting({
+    //         success(settingdata) {
+    //           console.log(settingdata)
+    //           if (settingdata.authSetting['scope.writePhotosAlbum']) {
+    //             console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
+    //           } else {
+    //             console.log('获取权限失败，给出不给权限就无法正常使用的提示')
+    //           }
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
 
   },
   /**
